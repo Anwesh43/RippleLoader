6e5e727a0860e6eb7c368ea03aafab381433e77f
Anwesh43 <anweshthecool0@gmail.com>
2016-11-30 14:57:11 +0530

    Chaning AppCompatActivitiy to Activity

--- rippleloader/src/main/java/com/anwesome/uiview/rippleloader/RippleLoader.java
+++ rippleloader/src/main/java/com/anwesome/uiview/rippleloader/RippleLoader.java
@@ -1,5 +1,6 @@
 package com.anwesome.uiview.rippleloader;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
@@ -10,16 +11,16 @@ import android.view.ViewGroup;
  * Created by anweshmishra on 30/11/16.
  */
 public class RippleLoader{
-    private AppCompatActivity appCompatActivity;
+    private Activity activity;
     private RippleLoaderView rippleLoaderView;
     private boolean isLoading = false;
-    public RippleLoader(AppCompatActivity appCompatActivity) {
-        this.appCompatActivity = appCompatActivity;
+    public RippleLoader(Activity appCompatActivity) {
+        this.activity = appCompatActivity;
     }
     public void startLoading() {
         if(!isLoading) {
-            rippleLoaderView = new RippleLoaderView(appCompatActivity.getApplicationContext());
-            this.appCompatActivity.addContentView(rippleLoaderView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            rippleLoaderView = new RippleLoaderView(activity.getApplicationContext());
+            this.activity.addContentView(rippleLoaderView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             isLoading = true;
         }
     }
